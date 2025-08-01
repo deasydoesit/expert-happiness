b6075e583969058da65b8bfb4bd2b84ba14179af
Darwin Rinderer <darwin.rinderer@gmail.com>
2025-08-01 16:39:56 -0400

    Updates to eth-kms-signer

--- eth-kms-signer/index.js
+++ eth-kms-signer/index.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 
-const { AwsKmsSigner } = require('eth-signer-kms');
+const { KMSClient } = require("@aws-sdk/client-kms");
+const { KMSSigner } = require('@rumblefishdev/eth-signer-kms');
 const { ethers } = require('ethers');
 
 async function main() {
+  const kms = new KMSClient({ region: process.env.AWS_REGION });
+
   const provider = new ethers.JsonRpcProvider(process.env.ETH_RPC_URL);
-  const signer = new AwsKmsSigner({
-    keyId: process.env.KMS_KEY_ID,
-    region: process.env.AWS_REGION,
-    provider,
-  });
+  const signer = await KMSSigner.create(provider, process.env.KMS_KEY_ID, kms);
 
   const address = await signer.getAddress();
   console.log("Using address:", address);
